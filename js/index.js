4e11e96c8ed725ca9a4864eeb63646805a62cde5
Shura2k18 <makcim2004kucheruk@gmail.com>
2019-01-29 22:58:05 +0200

    Titel site2

---
+++ js/index.js
@@ -0,0 +1,40 @@
+$(document).on('ready', function() {
+  $('.center').slick({
+    infinite: true,
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    responsive: [
+
+      {
+        breakpoint: 1100,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+});
